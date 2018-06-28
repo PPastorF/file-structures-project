02cdcbb7e16f11121b96ddd519ee23638bfe0c1b
Pedro Pastorello <pedropastor99@hotmail.com>
2018-06-27 22:36:35 -0300

    Update indice.c

--- indice.c
+++ indice.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include "manipulacaoArquivos2.h"
+#include "manipulacaoArquivos.h"
 #include "indice.h"
 
 // Funcao que retorna o RRN da pagina mae de uma pagina de rrn rrnPag
